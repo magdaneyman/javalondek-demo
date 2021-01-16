367fc217b7bc2cafb1d8e873df6ad2347d730ddd
magdaneyman <magda.neyman@gmail.com>
2021-01-16 11:29:31 +0100

    [init] - initial project commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+// Nie tworzmy klas w glownym pakiecie , nie bedzie do niego dostepu z innych pakietow
+    public static void main(String[] args) {
+        System.out.println("Wrong hello world");
+    }
+}
---
+++ src/main/java/pl/sda/javalondek4/java_demo/HelloYou.java
@@ -0,0 +1,12 @@
+package pl.sda.javalondek4.java_demo;
+// root package = groupID+artifactID
+public class HelloYou {
+
+    public static void main(String[] args) {
+        System.out.println("Witajcie na zajeciach u Manka");
+    }
+
+
+
+
+}

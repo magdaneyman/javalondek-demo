d499afcc9a413c96a12d16b1245df2a8a5eee85d
magdaneyman <magda.neyman@gmail.com>
2021-01-23 09:27:44 +0100

    [change ] -  minor changes in Player

--- src/main/java/pl/sda/javalondek4/java_demo/lambda/Player.java
+++ src/main/java/pl/sda/javalondek4/java_demo/lambda/Player.java
@@ -8,20 +8,17 @@ public class Player implements Playable{
     }
 
     Playable footballPlayer = ()->{
-        System.out.println("I can play footbal");
+        System.out.println("I can play football");
     };
 
 
     Playable gamesPlayer =() -> {
-        System.out.println( "Only playing games bring joy to your life");
+        System.out.println( "Only playing games brings joy to your life");
 
     };
     Playable looser =() -> {
         System.out.println(" Even when you loose you learn");
     };
-
-
-
 
 
 }
